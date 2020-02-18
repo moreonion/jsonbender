1b148d53840c1824f40eb9b39b3a1a7470dcda8a
Roman Zimmermann <roman@more-onion.com>
2020-02-18 09:01:17 +0100

    Add test for ProtectedFormat.

--- tests/test_string_ops.py
+++ tests/test_string_ops.py
@@ -1,7 +1,7 @@
 import unittest
 
 from jsonbender import K, S
-from jsonbender.string_ops import Format
+from jsonbender.string_ops import Format, ProtectedFormat
 from jsonbender.test import BenderTestMixin
 
 
@@ -11,6 +11,16 @@ class TestFormat(unittest.TestCase, BenderTestMixin):
                         K('This'), K('is'), K('a'),
                         noun=K('test'))
         self.assert_bender(bender, None, 'This is a test.')
+
+
+class TestProtectedFormat(unittest.TestCase):
+    def test_format(self):
+        bender = ProtectedFormat(
+            '{} {} {} {noun}.',
+            K('This'), K('is'), K('a'), noun=S('noun').optional(),
+        )
+        assert bender({}) is None
+        assert bender({'noun': 'test'}) == 'This is a test.'
 
 
 if __name__ == '__main__':
